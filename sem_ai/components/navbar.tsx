12f0d187076d2eb77b9abb131dba738d507645dc
fnabilz <fikrizamzak@gmail.com>
2026-06-11 21:53:16 +0800

    new navbar

--- sem_ai/components/navbar.tsx
+++ sem_ai/components/navbar.tsx
@@ -1,16 +1,25 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import logo_image from '../public/semai-logo.svg'
 
 export default function Navbar() {
     return (
-       <nav>
-            <div className='flex flex-col items-center justify-center pt-5 pb-5 shadow-md bg-transparent'>
-                <Image
-                    src={logo_image}
-                    width={150}
-                    alt="Logo of the system"
-                />
-            </div>
-        </nav>
+       <div className='object-center py-3 shadow-md inline-flex bg-background'>
+            <nav className='ml-10 flex font-medium'>
+                <ul className='w-2xl flex gap-8 text-lg items-center'>
+                    <li className=''><Link href='/'>Home</Link></li>
+                    <li className=''><Link href='/login'>Login</Link></li>
+                    <li className=''><Link href='/contacts'>Contacts</Link></li>
+                    <Image 
+                        className='mx-auto'
+                        src={logo_image}
+                        width={150}
+                        alt="Logo of the system"
+                    />
+                    <li><Link href=''></Link></li>
+                </ul>
+            </nav>
+            
+        </div>
     )
 }
