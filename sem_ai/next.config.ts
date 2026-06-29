606e87cc5eba3b63b98aac55cd52ccb0a2143d82
Amirul Hakim <amirulhazhar@gmail.com>
2026-06-30 00:22:35 +0800

    bar chart config

--- sem_ai/next.config.ts
+++ sem_ai/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["recharts"],
+  allowedDevOrigins: ['192.168.56.1']
 };
 
 export default nextConfig;
